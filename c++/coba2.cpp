2a2c2486aa02a110f62026f5e1f0f40f7c5e9d5d
ahmar <agambujang3@gmail.com>
2025-10-13 12:46:12 +0700

    Update coba2.cpp

---
+++ c++/coba2.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <conio.h>
+
+main()
+{
+
+    char nm1[50];
+    char nm2[50];
+
+    puts("Masukan nama pertama=");
+    gets(nm1);
+    puts("Masukan nama kedua=");
+    gets(nm2);
+
+    printf("\n\n");
+
+    puts("selamat datang");
+    gets(nm1);
+    puts("selamat datang");
+    gets(nm2);
+}
