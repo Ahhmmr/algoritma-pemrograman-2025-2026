bbd4fb25518a8ba4bdffcab486374cfbf6548b94
ahmar <agambujang3@gmail.com>
2025-10-13 10:46:51 +0700

    tugas 1

---
+++ c++/tugas1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+main()
+{
+
+    string nama = "Bayu";
+    int umur = 19;
+    float tinggi = 170.5;
+    bool aktif = true;
+
+    cout << "Nama: " << nama << endl;
+    cout << "Umur: " << umur << endl;
+    cout << "Tinggi: " << tinggi << endl;
+    cout << "Status Aktif: " << aktif << endl;
+    return 0;
+}
