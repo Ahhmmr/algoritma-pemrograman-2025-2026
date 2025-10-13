0af4cea5e629bef7cd16a77eff1193d30c14503d
ahmar <agambujang3@gmail.com>
2025-10-13 11:05:04 +0700

    coba 1

---
+++ c++/coba1.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <conio.h>
+
+main()
+{
+
+    int a = 6;
+    int b = 1;
+    char c[15] = "jokowi";
+
+    puts("nama: ");
+    puts(c);
+    printf("terdiri dari%dhuruf dan %Dkata, a,b");
+}
