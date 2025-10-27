6b963edf2dcb9806b3650774ceff71899823009f
Ahmar <agambujang3@gmail.com>
2025-10-27 11:12:44 +0700

    Add files via upload
    tugas

---
+++ angka.py
@@ -0,0 +1,11 @@
+a=int(input("masukan angka pertama:"))
+b=int(input("masukan angka kedua:"))
+if a>b:
+    print(f"{a} lebih besar dari {b}")
+
+elif a<b:
+    print(f"{a} lebih kecil dari {b}")
+
+else:
+    print('keduanya sama besar')
+    print('apakah keduanya sama besar?',a==b)
---
+++ variabel.py
@@ -0,0 +1,13 @@
+print('---BELAJAR VARIABEL---')
+
+char='A'
+string="Kamerad Kronos"
+a=38
+penghasilan=5000000
+nilai_uts=95.6
+
+print(char)
+print(string)
+print(a)
+print(penghasilan)
+print(nilai_uts)
